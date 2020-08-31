50903966efb97f7a9dda76815edff3c783ecbf67
juzhiyuan <juzhiyuan@apache.org>
2020-08-31 11:20:39 +0800

    feat: added GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document from "next/document";
+import Document, { Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -26,5 +26,30 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <html>
+        <body>
+          <Main />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-176821176-1"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-176821176-1');
+`,
+            }}
+          ></script>
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
