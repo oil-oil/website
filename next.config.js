904842968c516d769b66f2912bedc9cc29f54d35
juzhiyuan <juzhiyuan@apache.org>
2020-09-03 13:54:39 +0800

    feat(i18n): added localSubpaths

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const { nextI18NextRewrites } = require('next-i18next/rewrites')
 
-const localeSubpaths = {}
+const localeSubpaths = {
+  "en-US": 'en'
+}
 
 module.exports = {
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
