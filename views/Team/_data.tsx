558b4fb40c6b5e9b66a5bf23735c5a0570f5bb80
琚致远 <juzhiyuan@apache.org>
2021-04-16 14:31:27 +0800

    docs: added new member (#59)

--- views/Team/_data.tsx
+++ views/Team/_data.tsx
@@ -29,6 +29,12 @@ export const members: Member[] = [
     description: 'Apache APISIX Founder & PMC',
     type: 'team'
   }, {
+    role: 'Partner / Product VP',
+    image: 'https://api7-website-1301662268.file.myqcloud.com/team/members/wangyeliang-3.jpg',
+    name: 'Yeliang Wang',
+    description: '',
+    type: 'team'
+  }, {
     role: 'Technical Consultant',
     image:
       'https://www.tetrate.io/about/img/people/sheng-wu.jpg',
