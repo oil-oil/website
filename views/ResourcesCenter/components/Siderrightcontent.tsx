32856bf73fa4c3f8adac0145b75e9ed46c997a73
guoqqqi <979918879@qq.com>
2021-09-05 18:17:24 +0800

    style: update code

--- views/ResourcesCenter/components/Siderrightcontent.tsx
+++ views/ResourcesCenter/components/Siderrightcontent.tsx
@@ -13,7 +13,10 @@ import {
   SContentcard,
   SCardstyle,
   Siderright,
-  SContentlist
+  SContentlist,
+  SResourcesCard,
+  SLinkBox,
+  SCommunityBox
 } from "../style";
 
 type Props = {
@@ -21,11 +24,6 @@ type Props = {
 };
 
 const SiderRightContent: NextPage<Props, any> = ({ t }) => {
-  const titleList = [
-    { title: "RESOURCE CENTER" },
-    { title: "API7 Resources" },
-    { title: "Everything  you need to optimize the way you work with" },
-  ]
   const cardList = [
     {
       scontentcardbg: "#a06aff",
@@ -33,7 +31,7 @@ const SiderRightContent: NextPage<Props, any> = ({ t }) => {
       imgURL: "https://d2zv2ciw0ln4h1.cloudfront.net/uploads/book_icon_80d2c538d0_7c87cfcff3.svg",
       h1: "Developer",
       h1br: "Documentation",
-      h3: "All you need to get API7 up-and-running.",
+      desc: "All you need to get API7 up-and-running.",
       buttontext: "Documentation",
       buttonbg: "#773ae7",
     },
@@ -41,27 +39,22 @@ const SiderRightContent: NextPage<Props, any> = ({ t }) => {
       scontentcardbg: "#ef773c",
       divbgcolor: "#cc5d34",
       imgURL: "https://d2zv2ciw0ln4h1.cloudfront.net/uploads/user_guide_icon_8cbe401776_121b8a6c83.svg",
-      h1: "Developer",
-      h1br: "Documentation",
-      h3: "All you need to get API7 up-and-running.",
-      buttontext: "Documentation",
+      h1: "User",
+      h1br: "Guide",
+      desc: "All you need to get API7 up-and-running.",
+      buttontext: "User Guide",
       buttonbg: "#cc5d34",
     },
     {
       scontentcardbg: "#005abf",
       divbgcolor: "#01489e",
       imgURL: "https://d2zv2ciw0ln4h1.cloudfront.net/uploads/education_icon_18fb97653f_723577953c.svg",
-      h1: "Developer",
-      h1br: "Documentation",
-      h3: "All you need to get API7 up-and-running.",
-      buttontext: "Documentation",
+      h1: "API7",
+      h1br: "Academy",
+      desc: "All you need to get API7 up-and-running.",
+      buttontext: "API7 Academy",
       buttonbg: "#01489e",
     }
-  ]
-  const titleListSecond = [
-    { title: "" },
-    { title: "Learn with API7" },
-    { title: "Explore our library of educational SContent to learn how to set up and manage your API7 project." }
   ]
   const cardListSecond = [
     {
@@ -78,9 +71,7 @@ const SiderRightContent: NextPage<Props, any> = ({ t }) => {
       srcURL: "https://d2zv2ciw0ln4h1.cloudfront.net/uploads/education_color_icon_df7a108290_6a4ece3811.svg",
       text: "Academy",
       divcontent: "Learn how to become a API7 expert."
-    }
-  ]
-  const cardListThird = [
+    },
     {
       srcURL: "https://d2zv2ciw0ln4h1.cloudfront.net/uploads/recipe_starter_36c43126ce_065f8d503b.svg",
       text: "API7 Recipes",
@@ -97,16 +88,7 @@ const SiderRightContent: NextPage<Props, any> = ({ t }) => {
       divcontent: "Discover all the latest about our product and culture."
     }
   ]
-  const titleListThird = [
-    { title: "" },
-    { title: "Changelog" },
-    { title: "Find out about the API7 product updates, new features and general improvements." }
-  ]
-  const titleListFour = [
-    { title: "" },
-    { title: "Connect with the community" },
-    { title: "Join the conversation with users from all over the world." }
-  ]
+
   const cardListFour = [
     {
       srcURL: "https://d2zv2ciw0ln4h1.cloudfront.net/uploads/iconfinder_discord_3069758_1_1_18c962b4c9_b698a303d6.svg",
@@ -117,9 +99,7 @@ const SiderRightContent: NextPage<Props, any> = ({ t }) => {
       srcURL: "https://d2zv2ciw0ln4h1.cloudfront.net/uploads/forum_14cfcb6fc1_3d87b46985.png",
       text: "Forum",
       divcontent: "Find answers and ask questions to the community"
-    }
-  ]
-  const cardListFive = [
+    },
     {
       srcURL: "https://d2zv2ciw0ln4h1.cloudfront.net/uploads/github_a0fb0e8472_2f1a9ca3d9.png",
       text: "Github",
@@ -131,6 +111,7 @@ const SiderRightContent: NextPage<Props, any> = ({ t }) => {
       divcontent: "Stay up to date with the latest news"
     }
   ]
+
   const cardListSix = [
     {
       scontentcardbg: "#a06aff",
@@ -156,273 +137,112 @@ const SiderRightContent: NextPage<Props, any> = ({ t }) => {
   return (
     <>
       <NextSeo title={t(`common:contentlast`)} />
-      <Siderright style={{ display: 'flex', flexDirection: 'column' }}>
+      <Siderright>
         <SContentitem>
           <STitle>
-            {titleList.map((item) => (
-              <div>{item.title}</div>
-            ))}
+            <span>RESOURCE CENTER</span>
+            <h2>API7 Resources</h2>
+            <p>Everything  you need to optimize the way you work with</p>
           </STitle>
-          <SCardstyle>
+          <SResourcesCard>
             {cardList.map((item) => (
-              <SContentcard style={{ background: (item.scontentcardbg) }}>
-                <div style={{
-                  borderRadius: 40,
-                  width: 80,
-                  height: 80,
-                  backgroundColor: (item.divbgcolor),
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-
-                }}>
-                  <img src={item.imgURL} alt="" style={{ width: 35, height: 35, }} />
+              <SContentcard key={item.h1} style={{ background: (item.scontentcardbg) }}>
+                <div style={{ backgroundColor: (item.divbgcolor) }}>
+                  <img src={item.imgURL} alt="" />
                 </div>
-                <h1 style={{ color: "white", fontWeight: 700, fontSize: 20, marginTop: 15 }}>{item.h1}<br />
-                  {item.h1br}
-                </h1>
-                <h3 style={{ fontSize: 16, marginTop: 15, marginBottom: 15 }}>{item.h3}</h3>
+                <h1>{item.h1}<br />{item.h1br}</h1>
+                <p>{item.desc}</p>
                 <Button style={{ backgroundColor: (item.buttonbg), color: "white", marginTop: 30, height: 50 }}>{item.buttontext}</Button>
               </SContentcard>
             ))}
-          </SCardstyle>
+          </SResourcesCard>
         </SContentitem>
-        <SContentitem style={{ marginTop: 70 }}>
-          <STitle>
-            {titleListSecond.map((item) => (
-              <div>{item.title}</div>
-            ))}
-          </STitle>
 
-          <SCardstyle style={{ display: "flex", flexDirection: 'row', }}>
+        <SContentitem>
+          <STitle>
+            <h2>Learn with API7</h2>
+            <p>Explore our library of educational SContent to learn how to set up and manage your API7 project.</p>
+          </STitle>
+          <SLinkBox>
             {cardListSecond.map((item) => (
-              <div style={{ flex: 1 }}>
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: 72,
-                    height: 72,
-                    position: 'relative',
-                    backgroundColor: '#f6fafe',
-                    borderRadius: 5
-                  }}
-                ><img style={{ position: 'absolute', left: 20 }}
-                  src={item.srcURL} alt="" />
-                  <div style={{ position: 'absolute', left: 90, display: "flex", flexDirection: "column", width: 300 }}>
-                    <Text style={{ color: "#1d1b84", fontWeight: 700 }}>{item.text}</Text>
-                    <div style={{
-                      width: 165, fontSize: 14,
-                      fontWeight: 400, color: "#1d1b84",
-                    }}>{item.divcontent}</div>
-                  </div>
+              <a href="" className="box">
+                <div className="iconBox">
+                  <div></div>
+                  <img src={item.srcURL} alt="" />
                 </div>
-              </div>
-            ))}
-          </SCardstyle>
-          <SCardstyle style={{ display: "flex", flexDirection: 'row', marginTop: 30 }}>
-            {cardListThird.map((item) => (
-              <div style={{ flex: 1 }}>
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: 72,
-                    height: 72,
-                    position: 'relative',
-                    backgroundColor: '#f6fafe',
-                    borderRadius: 5
-                  }}
-                ><img style={{ position: 'absolute', left: 20 }}
-                  src={item.srcURL} alt="" />
-                  <div style={{ position: 'absolute', left: 90, display: "flex", flexDirection: "column", width: 300 }}>
-                    <Text style={{ color: "#1d1b84", fontWeight: 700 }}>{item.text}</Text>
-                    <div style={{
-                      width: 165, fontSize: 14,
-                      fontWeight: 400, color: "#1d1b84",
-                    }}>{item.divcontent}</div>
-                  </div>
+                <div className="content">
+                  <Text style={{ fontSize: 17, color: "#1d1b84", fontWeight: 700 }}>{item.text}</Text>
+                  <p>{item.divcontent}</p>
                 </div>
-              </div>
+              </a>
             ))}
-          </SCardstyle>
+          </SLinkBox>
         </SContentitem>
+
         <SContentitem>
           <STitle style={{ marginBottom: 40 }}>
-            {titleListThird.map((item) => (
-              <div>{item.title}</div>
-            ))}
+            <h2>Changelog</h2>
+            <p>Find out about the API7 product updates, new features and general improvements.</p>
+            <Button style={{ marginTop: '58px', backgroundColor: '#8c4bff', width: 250, height: 55, color: 'white' }}>See the changelogs</Button>
           </STitle>
           <SCardstyle>
-            <Button style={{ backgroundColor: '#8c4bff', width: 250, height: 55, color: 'white' }}>See the changelogs</Button>
-          </SCardstyle>
-          <SCardstyle style={{
-            marginTop: 50,
-            marginBottom: 40
-          }}>
             <div style={{
               display: "flex",
               flexDirection: "column",
               marginTop: 15
-
             }}>
-              <div style={{
-                backgroundColor: "#c8cee1",
-                width: 10,
-                height: 10,
-                borderRadius: 5,
-                marginLeft: 50,
-              }}></div>
-              <div style={{
-                backgroundColor: "#c8cee1",
-                width: 2,
-                height: 65,
-                marginLeft: 54,
-                marginTop: 6,
-                marginBottom: 3
-              }}>
+              <div className="linePoint"></div>
+              <div className="timeLine"></div>
 
+              <div className="linePoint"></div>
+              <div className="timeLine"></div>
+
+              <div className="linePoint"></div>
+              <div className="timeLine"></div>
+            </div>
+            <div className="timeLineContent">
+              <div className="timeContentBox">
+                <div>
+                  <p>Aug 4, 2021</p>
+                  <span>Improvements and fixes - API7 v3.6.6 & v3.6.7</span>
+                </div>
               </div>
-              <div style={{
-                backgroundColor: "#c8cee1",
-                width: 10,
-                height: 10,
-                borderRadius: 5,
-                marginLeft: 50,
-              }}></div>
-              <div style={{
-                backgroundColor: "#c8cee1",
-                width: 2,
-                height: 65,
-                marginLeft: 54,
-                marginTop: 6,
-                marginBottom: 3
-              }}>
+              <div className="timeContentBox">
+                <div>
+                  <p>Jun 23, 2021</p>
+                  <span>3.6.5 Improvements and fixes - API7 v3.6.5</span>
+                </div>
               </div>
-              <div style={{
-                backgroundColor: "#c8cee1",
-                width: 10,
-                height: 10,
-                borderRadius: 5,
-                marginLeft: 50,
-              }}></div>
-              <div style={{
-                backgroundColor: "#c8cee1",
-                width: 2,
-                height: 45,
-                marginLeft: 54,
-                marginTop: 6,
-                marginBottom: 3
-              }}>
+              <div className="timeContentBox">
+                <div>
+                  <p>Apr 22, 2021</p>
+                  <span>🌍 Internationalization - API7 v3.6</span>
+                </div>
               </div>
             </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                position: "absolute"
-              }}>
-              <div style={{
-                height: 90, width: 700, marginLeft: 100
-              }}>
-                Aug 4, 2021<br />
-                Improvements and fixes - API7 v3.6.6 & v3.6.7
-              </div>
-              <div style={{
-                height: 90, width: 700, marginLeft: 100
-
-              }}>
-                Jun 23, 2021<br />
-                3.6.5
-                Improvements and fixes - API7 v3.6.5
-              </div>
-              <div style={{
-                height: 90, width: 700, marginLeft: 100
-              }}>
-                Apr 22, 2021<br />
-                🌍 Internationalization - API7 v3.6
-              </div>
-            </div>
-
           </SCardstyle>
         </SContentitem>
+
         <SContentitem>
           <STitle style={{ marginBottom: 40 }}>
-            {titleListFour.map((item) => (
-              <div>{item.title}</div>
-            ))}
+            <h2>Connect with the community</h2>
+            <p>Join the conversation with users from all over the world.</p>
           </STitle>
-          <SCardstyle style={{ display: "flex", flexDirection: 'row', }}>
+          <SCommunityBox>
             {cardListFour.map((item) => (
-              <div style={{ flex: 1 }}>
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: 420,
-                    height: 80,
-                    position: 'relative',
-                    backgroundColor: '#f6fafe',
-                    borderRadius: 15,
-                    marginRight: 10,
-                  }}
-                >
-                  <img style={{ position: 'absolute', left: 20 }}
-                    src={item.srcURL} />
-                  <div style={{ position: 'absolute', left: 90, display: "flex", flexDirection: "column", }}>
-                    <Text style={{
-                      color: "#1d1b84", fontSize: 23,
-                      fontWeight: 500
-                    }}>{item.text}</Text>
-                    <div style={{
-                      color: "#1d1b84", fontSize: 17,
-                      fontWeight: 400
-                    }}>{item.divcontent}</div>
-                  </div>
+              <a key={item.text} className="communityList">
+                <div className="background"></div>
+                <div className="iconBox">
+                  <img src={item.srcURL} />
                 </div>
-              </div>
+                <div className="contentBox">
+                  <Text style={{color: "#1d1b84", fontSize: 17, fontWeight: 600, lineHeight: 1.5 }}>{item.text}</Text>
+                  <p>{item.divcontent}</p>
+                </div>
+              </a>
             ))}
-          </SCardstyle>
-          <SCardstyle style={{ display: "flex", flexDirection: 'row', }}>
-            {cardListFive.map((item) => (
-              <div style={{ flex: 1 }}>
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: 420,
-                    height: 80,
-                    position: 'relative',
-                    backgroundColor: '#f6fafe',
-                    borderRadius: 15,
-                    marginRight: 10,
-                  }}
-                >
-                  <img style={{ position: 'absolute', left: 20 }}
-                    src={item.srcURL} alt="" />
-                  <div style={{ position: 'absolute', left: 90, display: "flex", flexDirection: "column", }}>
-                    <Text style={{
-                      color: "#1d1b84", fontSize: 23,
-                      fontWeight: 600
-                    }}>{item.text}</Text>
-                    <div style={{
-                      color: "#1d1b84", fontSize: 17,
-                      fontWeight: 400
-                    }}>{item.divcontent}</div>
-                  </div>
-                </div>
-              </div>
-            ))} </SCardstyle>
+          </SCommunityBox>
+
           <SCardstyle>
             {cardListSix.map((item) => (
               <SContentcard style={{ background: (item.scontentcardbg), }}>
@@ -443,7 +263,7 @@ const SiderRightContent: NextPage<Props, any> = ({ t }) => {
             ))}
           </SCardstyle>
         </SContentitem>
-        <SContentitem style={{ textAlign: "center", marginTop: 50 }}>
+        <SContentitem>
           <SContentlist>
             <div style={{
               width: 320,
--- views/ResourcesCenter/style.ts
+++ views/ResourcesCenter/style.ts
@@ -78,58 +78,187 @@ export const SNavbarstart = styled.div`
   width: 100px;
 `;
 
-export const SNavbarsearch = styled.div`
+export const SCardstyle = styled.div`
   display: flex;
   flex-direction: row;
-  height: 80px;
-  position: sticky;
   width: 100%;
-  justify-content: center;
-  align-items: center;
-  border-bottom: 2px solid #eaf2fa;
-  div:nth-child(1) {
-    position: relative;
-    width: 15%;
-    margin-left: -160px;
-    top: 25%;
-    font-size: 21px;
-    font-weight: 700;
+  .linePoint {
+    background-color: #c8cee1;
+    width: 10px;
+    height: 10px;
+    border-radius: 5px;
+    margin-left: 50px;
   }
-  div:nth-child(2) {
+  .timeLine {
+    background-color: #c8cee1;
+    width: 2px;
+    height: 65px;
+    margin: 6px 0 3px 54px;
+  }
+
+  .timeLineContent {
     display: flex;
-    position: relative;
-    width: 50%;
-    height: 50px;
-    margin-top: 15px;
-    margin-left: 80px;
-    border-radius: 15px;
-    input {
-      position: relative;
-      width: 100%;
-      z-index: 99;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+  }
+
+  .timeContentBox {
+    height: 90px;
+    width: 100%;
+    margin-left: 200px;
+    display: flex;
+    align-items: center;
+    p {
+      line-height: 19px;
+      font-family: Poppins, serif;
+      font-weight: 400;
+      color: #344b80;
+    }
+    span {
+      font-size: 17px;
+      line-height: 22px;
+      font-weight: 600;
+      color: #1d1b84;
     }
   }
 `;
 
-export const SCardstyle = styled.div`
+export const SResourcesCard = styled.div`
   display: flex;
   flex-direction: row;
+  width: 100%;
+`;
+
+export const SLinkBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 20px;
+  width: 100%;
+
+  @media (max-width: 900px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (max-width: 700px) {
+    grid-template-columns: 1fr;
+  }
+
+  .box {
+    display: flex;
+    box-sizing: border-box;
+    text-align: left;
+    width: 100%;
+    text-decoration: none;
+    border-radius: 10px;
+
+    .iconBox {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-width: 72px;
+      width: 72px;
+      height: 72px;
+      margin-right: 16px;
+      position: relative;
+
+      div {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        background-color: #f6fafe;
+        border: 1px solid #eaf2fa;
+        border-radius: 10px;
+        z-index: -1;
+      }
+    }
+
+    .content {
+      flex: 1 1;
+      padding-top: 4px;
+
+      p {
+        font-size: 13px;
+        margin-top: 4px;
+      }
+    }
+  }
+`;
+
+export const SCommunityBox = styled.div`
+  display: grid;
+  width: 100%;
+  grid-gap: 23px;
+  grid-template-columns: 1fr 1fr;
+  margin: 34px 0 48px;
+
+  @media (max-width: 900px) {
+    grid-template-columns: 1fr;
+  }
+
+  .communityList {
+    display: flex;
+    position: relative;
+    align-items: center;
+    text-decoration: none;
+    border: 10px;
+    text-align: left;
+    width: 100%;
+    box-sizing: border-box;
+  }
+  .iconBox {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 72px;
+    width: 72px;
+    height: 72px;
+    position: relative;
+    background-color: #f6fafe;
+    border-radius: 15px;
+    margin-right: 10px;
+  }
+
+  .background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    background-color: #f6fafe;
+    border: 1px solid #eaf2fa;
+    border-radius: 10px;
+    z-index: -1;
+  }
+
+  .contentBox {
+    padding: 18px 18px 18px 0;
+    p {
+      font-size: 13px;
+      font-weight: 400;
+      color: #344b80;
+      font-family: Poppins,serif;
+      line-height: 19px;
+    }
+  }
 `;
 
 export const SContent = styled.div`
   display: flex;
   flex-direction: row;
   position: relative;
-  width: 100%;
+  margin: 0 auto;
+  width: 83.333%;
   margin-bottom: 20px;
   border: white;
 `;
 
 export const SiderLeft = styled.div`
   display: flex;
+  flex: 1 1;
+  min-width: 215px;
   flex-direction: column;
-  margin-left: 9%;
-  width: 16%;
   margin-top: 20px;
   font-size: 17px;
   font-weight: 400;
@@ -144,20 +273,24 @@ export const SiderLeft = styled.div`
     img {
     }
   }
+
+  @media (max-width: 1200px) {
+    display: none;
+  }
 `;
 
 export const SContentitem = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
   position: relative;
   margin-left: 4%;
+  margin-bottom: 64px;
 `;
 
 export const Siderright = styled.div`
   display: flex;
+  flex: 0 1 auto;
   flex-direction: column;
-  width: 66%;
   position: relative;
   margin-left: 1%;
 `;
@@ -167,12 +300,8 @@ export const STitle = styled.div`
   flex-direction: column;
   position: relative;
   margin-top: 50px;
-  div:nth-child(1) {
-    font-size: 12px;
-    font-weight: 700;
-    margin-bottom: 35px;
-    color: #8c4bff;
-  }
+  margin-bottom: 64px;
+
   div:nth-child(2) {
     font-size: 30px;
     position: relative;
@@ -186,13 +315,36 @@ export const STitle = styled.div`
     font-weight: 400;
     color: #344b80;
   }
+  span {
+    font-size: 13px;
+    font-weight: 700;
+    margin-bottom: 35px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    color: #8c4bff;
+  }
+
+  h2 {
+    font-size: 43px;
+    position: relative;
+    margin-bottom: 35px;
+    font-weight: 700;
+    line-height: 1.19;
+    transform: translateZ(1000px);
+    color: #1d1b84;
+  }
+
+  p {
+    font-size: 17px;
+    color: #344b80;
+    font-weight: 400;
+    line-height: 28px;
+  }
 `;
 
 export const SContentcard = styled.div`
   display: flex;
   flex-direction: column;
-  width: 20%;
-  position: relative;
   top: 70px;
   margin-right: 15px;
   border-radius: 10px;
@@ -200,9 +352,30 @@ export const SContentcard = styled.div`
   box-sizing: border-box;
   text-align: left;
   width: 100%;
+
+  h1 {
+    color: #ffffff;
+    font-weight: 700;
+    font-size: 20px;
+    margin-top: 15px;
+  }
+  p {
+    color: #ffffff;
+    font-size: 16px;
+    margin: 15px 0;
+  }
+  div {
+    border-radius: 40px;
+    width: 80px;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
   img {
-    width: 30px;
-    height: 30px;
+    width: 35px;
+    height: 35px;
     opacity: 1;
     visibility: inherit;
   }
