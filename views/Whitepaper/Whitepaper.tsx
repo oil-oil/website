fe965df9f290c23fe5d58556ef702cb51e841f34
guoqqqi <72343596+guoqqqi@users.noreply.github.com>
2021-09-09 16:50:30 +0800

    docs: add API7 whitepaper for EN (#279)

--- views/Whitepaper/Whitepaper.tsx
+++ views/Whitepaper/Whitepaper.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { NextPage } from 'next';
-import { TFunction } from "next-i18next";
+import { I18nContext, TFunction } from "next-i18next";
 import { NextSeo } from "next-seo";
 import MobileDetect from "mobile-detect";
 
@@ -13,14 +13,19 @@ type Props = {
 
 const Whitepaper: NextPage<Props, any> = ({ t }) => {
   const [isMobile, setIsMobile] = useState(true);
-  const targetURL = "https://static.apiseven.com/202108/API7-Whitepaper.pdf"
+  const {
+    i18n: { language },
+  } = useContext(I18nContext);
+  const targetURL_ZH = "https://static.apiseven.com/202108/API7-Whitepaper.pdf"
+  const targetURL_EN = "https://static.apiseven.com/202108/API7-WhitePaper-EN.pdf"
+  const targetLink = language === "zh-CN" ? targetURL_ZH : targetURL_EN
 
   useEffect(() => {
     setIsMobile(false);
     const md = new MobileDetect(window.navigator.userAgent);
     if (md.mobile()) {
       setIsMobile(true);
-      window.location.href = targetURL;
+      window.location.href = targetLink;
     };
   }, []);
 
@@ -32,7 +37,7 @@ const Whitepaper: NextPage<Props, any> = ({ t }) => {
     <SWrapper>
       <NextSeo title={t(`common:whitepaper`)} />
       <SBox>
-        <iframe width="100%" height="800px" src={`${targetURL}#view=fitH`} itemType="application/pdf"></iframe>
+        <iframe width="100%" height="800px" src={`${targetLink}#view=fitH`} itemType="application/pdf"></iframe>
       </SBox>
     </SWrapper>
   );
