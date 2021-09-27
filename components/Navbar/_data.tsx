92f2669e576aa4e11f67d924e4fa0a6be4c63b00
guoqqqi <72343596+guoqqqi@users.noreply.github.com>
2021-09-27 21:41:08 +0800

    feat: add API7 Performance Report (#313)

--- components/Navbar/_data.tsx
+++ components/Navbar/_data.tsx
@@ -4,7 +4,7 @@ import { AiFillGithub, AiOutlineProject, AiOutlineTeam } from 'react-icons/ai'
 import { MdWeb } from 'react-icons/md'
 import { CgReadme } from 'react-icons/cg'
 import { SiMeetup } from 'react-icons/si'
-import { MdCompare } from 'react-icons/md'
+import { MdCompare, MdPoll } from 'react-icons/md'
 import { TiNews } from 'react-icons/ti'
 
 import { getRequestDemoLink } from '../../helper'
@@ -71,7 +71,12 @@ export const EN_US_Links: Link[] = [
         description: 'Learn more about the API7\'s Whitepaper for more features and performance reports.',
         href: 'https://static.apiseven.com/202108/API7-WhitePaper-EN.pdf',
         icon: <IoHelpBuoy />,
-      }
+      }, {
+        label: 'API7 Performance Report',
+        description: 'Learn more about the API7\'s Performance Report',
+        href: 'https://static.apiseven.com/202108/API7-Performance-Test-Report-EN.pdf',
+        icon: <MdPoll />,
+      },
     ],
   },
   {
@@ -182,7 +187,13 @@ export const ZH_CN_Links: Link[] = [
         description: '阅读 API7 网关技术白皮书，了解更多功能与性能报告。',
         href: 'https://static.apiseven.com/202108/API7-Whitepaper.pdf',
         icon: <CgReadme />,
-      }
+      },
+      {
+        label: '性能评测报告',
+        description: '阅读 API7 性能评测报告，了解更详细的性能测试结果。',
+        href: 'https://static.apiseven.com/202108/API7-Performance-Test-Report-ZH.pdf',
+        icon: <MdPoll />,
+      },
     ],
   }, {
     label: "商业支持",
