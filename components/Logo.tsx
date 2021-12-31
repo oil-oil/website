4566e52dcb0f496d9ae60104a1ee669f2b035393
guoqqqi <72343596+guoqqqi@users.noreply.github.com>
2021-12-31 15:10:07 +0800

    style: use the new API7.ai branding logo (#440)
    Co-authored-by: 琚致远 <juzhiyuan@apache.org>

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,10 @@
-import { HTMLChakraProps, Box, Image } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 import * as React from "react";
 
 export const Logo = () => {
   return (
     <Box as="a" href="/">
-      <Image src="https://static.apiseven.com/202108/api7-logo.png" alt="Logo" width="100px" loading="lazy" />
+      <Image src='https://static.apiseven.com/202108/1640917868852-37633689-5279-48d6-a13a-189054e4d15b.png' alt="Logo" width="130px" loading="lazy" />
     </Box>
   );
 };
